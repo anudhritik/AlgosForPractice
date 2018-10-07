70550f122917a7f5c31e96690aefbd257a413055
Anudhriti <dhritireddy28@gmail.com>
2018-10-07 18:18:52 -0400

    Minor change

--- src/com/programming/arrays/ArrayReversal.java
+++ src/com/programming/arrays/ArrayReversal.java
@@ -17,7 +17,7 @@ public class ArrayReversal {
 
 	public static void main(String[] args) {
 		int arr[] = { 1, 2, 3, 4 };
-		reverseArray(arr, 0, 3);
+		reverseArray(arr, 0, arr.length-1);
 		System.out.println("Reversed array: " + Arrays.toString(arr));
 	}
 }
