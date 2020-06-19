f13c8510a47580b69e8b397d2fc9151482f66fed
Anudhriti <anukreddy28@gmail.com>
2020-06-19 10:12:34 -0700

    Renaming

--- src/com/programming/linkedlist/LLImpl.java
+++ src/com/programming/linkedlist/LLImpl.java
@@ -25,11 +25,11 @@ public class LLImpl {
 	}
 
 	public void insertAtStart(int data) {
-		Node node = new Node();
-		node.data = data;
-		node.next = null;
-		node.next = head;
-		head = node;
+		Node newNode = new Node();
+		newNode.data = data;
+		newNode.next = null;
+		newNode.next = head;
+		head = newNode;
 
 	}
 
