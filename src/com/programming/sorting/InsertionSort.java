943143a8df801d26f780674b99f08fc0155e9e6c
Anudhriti <anukreddy28@gmail.com>
2020-06-23 00:00:51 -0700

    Insertion Sort

---
+++ src/com/programming/sorting/InsertionSort.java
@@ -0,0 +1,23 @@
+package com.programming.sorting;
+
+public class InsertionSort {
+	public static void insertionSort(int arr[]) {
+		for (int i = 1; i < arr.length; i++) {
+			int key = arr[i];
+			int j = i;
+			while (j > 0 && arr[j - 1] > arr[j]) {
+				arr[j] = arr[j - 1];
+				j--;
+			}
+			arr[j] = key;
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 5, 2, 4, 5, 8 };
+		InsertionSort.insertionSort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
