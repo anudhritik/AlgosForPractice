dc42b3ef9a25434e4f715409b464c3c045e1455d
Anudhriti <anukreddy28@gmail.com>
2020-06-23 16:27:59 -0700

    Insertion Sort/R

--- src/com/programming/sorting/InsertionSort.java
+++ src/com/programming/sorting/InsertionSort.java
@@ -5,7 +5,7 @@ public class InsertionSort {
 		for (int i = 1; i < arr.length; i++) {
 			int key = arr[i];
 			int j = i;
-			while (j > 0 && arr[j - 1] > arr[j]) {
+			while (j > 0 && arr[j - 1] > key) {
 				arr[j] = arr[j - 1];
 				j--;
 			}
@@ -14,7 +14,7 @@ public class InsertionSort {
 	}
 
 	public static void main(String[] args) {
-		int arr[] = { 1, 5, 2, 4, 5, 8 };
+		int arr[] = { 3, 5, 2, 4, 5, 8, 6, 1 };
 		InsertionSort.insertionSort(arr);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
