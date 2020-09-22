35ecc54bf428d60c4143f86af49816d4efb5d0e6
Anudhriti <anukreddy28@gmail.com>
2020-09-22 16:07:07 -0700

    More leetcode questions

---
+++ src/com/programming/leetcode/NumberOfSteps.java
@@ -0,0 +1,21 @@
+package com.programming.leetcode;
+
+public class NumberOfSteps {
+	public static int numberOfSteps(int num) {
+		int counter = 0;
+		while (num != 0) {
+			if (num % 2 == 0) {
+				num = num / 2;
+				counter++;
+			} else {
+				num = num - 1;
+				counter++;
+			}
+		}
+		return counter;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(numberOfSteps(14));
+	}
+}
